2d92bd11c51635bcb460c73ac70fc02f9468ebba
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-14 15:59:17 -0300

    feat: new controller method for get all users stored in database

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -3,6 +3,7 @@ import { getRepository } from 'typeorm';
 import User from '../models/User';
 
 class UserController {
+    
     async store(req: Request, res: Response) {
 
         const { email, password } = req.body;
@@ -20,6 +21,15 @@ class UserController {
         await userRepo.save(user)
 
         return res.json(user);
+    }
+
+    async getAll(req: Request, res: Response) {
+
+            const userRepo = getRepository(User);
+
+            const users = await userRepo.find();
+
+            return res.json(users)
     }
 }
 
