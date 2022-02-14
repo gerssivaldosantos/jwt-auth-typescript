a493d367f7fc6f3b0b3088f3c766f675cdc0fcb9
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-14 16:52:20 -0300

    feat: encrypt passord with bcryptjs

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,14 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
-
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate} from 'typeorm';
+import bcrypt from 'bcryptjs';
 @Entity('users')
 class User {
 
     @PrimaryGeneratedColumn('uuid')
     id: string;
+
     @Column()
     email: string;
+
     @Column()
     password: string;
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    hashPassword() {
+        this.password = bcrypt.hashSync(this.password, 8);
+    }
      
 }
 
