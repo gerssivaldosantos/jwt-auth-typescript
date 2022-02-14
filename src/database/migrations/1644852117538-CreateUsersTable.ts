aea1e2df7f2e9c8ea104f111a4805d29bdab3ee8
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-14 13:40:57 -0300

    feat: migration to create users table

---
+++ src/database/migrations/1644852117538-CreateUsersTable.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsersTable1644852117538 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
+        )
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name : "id",
+                        type: "uuid",
+                        isPrimary: true,
+                        generationStrategy: "uuid",
+                        default: 'uuid_generate_v4()'
+
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        isUnique: true
+                    },
+                    {
+                        name: "password",
+                        type: "varchar"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable(
+            'users'
+        )
+        await queryRunner.query('DROP EXTESION "uuid-ossp"')
+    }
+
+}
