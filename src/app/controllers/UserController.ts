5233004453f33d373ff57e7562dd67d66e3ba828
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-14 16:52:57 -0300

    feat: controller for delete user in database based in her id

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 import User from '../models/User';
 
 class UserController {
-    
+
     async store(req: Request, res: Response) {
 
         const { email, password } = req.body;
@@ -25,12 +25,52 @@ class UserController {
 
     async getAll(req: Request, res: Response) {
 
-            const userRepo = getRepository(User);
+        const userRepo = getRepository(User);
 
-            const users = await userRepo.find();
+        const users = await userRepo.find();
 
-            return res.json(users)
+        return res.json(users)
     }
+
+    async update(req: Request, res: Response) {
+        const userRepo = getRepository(User);
+        const { id } = req.params;
+        const { email, password } = req.body;
+        const user = await userRepo.findOne(id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        user.email = email;
+        user.password = password;
+        try {
+            await userRepo.save(user);
+        }
+        catch (error) {
+            return res.status(500).json({ message: error });
+        }
+
+        return res.json(user);
+
+
+    }
+
+    async delete(req: Request, res: Response) {
+        const userRepo = getRepository(User);
+        const { id } = req.params;
+        const user = await userRepo.findOne(id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        try {
+            await userRepo.remove(user);
+            return res.status(200).json({ message: 'User deleted' });
+        }
+        catch (error) {
+            return res.status(500).json({ message: error });
+        }
+    }
+
+
 }
 
 export default new UserController();
