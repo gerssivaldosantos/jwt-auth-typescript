84ea0e483d4b93d6a8f0e50deb7e8df2376e37e2
gerssivaldosantos <canalandows@gmail.com>
2022-02-14 11:25:48 -0300

    build: express config

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(8085, () => console.log('😀 Server started on port 8085'));
