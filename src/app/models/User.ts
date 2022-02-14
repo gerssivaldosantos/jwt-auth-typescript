4d49338bb87ca5111acdf1c762cde5709f181517
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-14 14:46:58 -0300

    refactor: removing unecessary parameters

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,12 +1,13 @@
 import { Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
+
 @Entity('users')
 class User {
 
     @PrimaryGeneratedColumn('uuid')
     id: string;
-    @Column('text')
+    @Column()
     email: string;
-    @Column('text')
+    @Column()
     password: string;
      
 }
