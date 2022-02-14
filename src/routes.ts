b8825038fb81643060a4143e249486e514a51159
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-14 16:52:42 -0300

    feat: route for call delete method in user controller

--- src/routes.ts
+++ src/routes.ts
@@ -6,4 +6,7 @@ const router = Router();
 
 router.post('/user', UserController.store);
 router.get('/users', UserController.getAll);
+router.put('/user/:id', UserController.update);
+router.delete('/user/:id', UserController.delete);
+
 export default router;
