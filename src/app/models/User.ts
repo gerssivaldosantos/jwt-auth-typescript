37e1f01e37f0fdc3faefbd6f4983c639021d8f20
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-14 13:59:24 -0300

    feat: entity setup

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,9 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
+@Entity('users')
 class User {
 
+    @PrimaryGeneratedColumn('uuid')
     id: string;
+    @Column('text')
     email: string;
+    @Column('text')
     password: string;
-    
+     
 }
 
 export default User;
