65e6f54a8ce2e273ee3495c0a1809a5ac457a793
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-14 16:03:12 -0300

    feat: new route to call the getAll method in user controller

--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,5 @@ import UserController from './app/controllers/UserController'
 const router = Router();
 
 router.post('/user', UserController.store);
-
+router.get('/users', UserController.getAll);
 export default router;
