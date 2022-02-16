1c9b151364ba3dc4d610d9baf7b180b7389cf248
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-16 10:07:35 -0300

    feat: token verification and data payload saved

--- src/app/middlewares/AuthMiddleware.ts
+++ src/app/middlewares/AuthMiddleware.ts
@@ -1,13 +1,39 @@
 import {Request, Response, NextFunction} from 'express';
+import jwt from 'jsonwebtoken';
 
+interface TokenPayload{
+    id: string;
+    iat: number;
+    exp: number;
+}
 class AuthMiddleware{
+    
+    formatToken(token: string){
+        return token.replace('Bearer ', '').trim();
+    }
+
     check(req: Request, res: Response, next: NextFunction){
-        
-        if(req.headers.authorization){
+
+        const { authorization } = req.headers;
+
+        try{
+            if(!authorization){
+                return res.status(401).json({
+                    error: 'Token not provided'
+                })
+            }
+
+            const secret = process.env.JWT_SECRET_KEY?? "";
+            const token = this.formatToken(authorization);
+            const data = jwt.verify(token,secret);
+            const {id, iat, exp} = data as TokenPayload;
             return next();
         }
-        else{
-            return res.status(401).json({ message: 'Unauthorized' });
+       
+        catch(err){
+            return res.status(401).json({
+                error: err
+            })
         }
     }
 }
