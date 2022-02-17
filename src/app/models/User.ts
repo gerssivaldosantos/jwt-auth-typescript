c737872df68536cf94bfe8e45380dbc47a00c0a9
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-17 14:32:58 -0300

    feat: insert the validator decorators

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate} from 'typeorm';
+import { IsEmail, IsNotEmpty, Length } from 'class-validator'
 import bcrypt from 'bcryptjs';
 @Entity('users')
 class User {
@@ -6,9 +7,11 @@ class User {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @IsEmail()
     @Column()
     email: string;
 
+    @Length(6, 40)
     @Column({select: false})
     password: string;
 
