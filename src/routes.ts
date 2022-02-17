8f48bb815753c8b13049dccc583ea13b0330528e
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-17 14:33:09 -0300

    feat: insert the middleware in create user route

--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,11 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController'
 import AuthController from './app/controllers/AuthController'
 import AuthMiddleware from './app/middlewares/AuthMiddleware';
+import ValidateMiddleware from './app/middlewares/ValidateMiddleware';
 const router = Router();
 
 /* Basic actions */
-router.post('/user', UserController.store);
+router.post('/user', ValidateMiddleware.validateSyntax ,UserController.store);
 router.get('/users', AuthMiddleware.checkToken, UserController.getAll);
 router.put('/user/:id', UserController.update);
 router.delete('/user/:id', UserController.delete);
