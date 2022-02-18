3711ff54ae566ba55235ccf616b7011436ef598a
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-18 16:40:01 -0300

    feat: send email to user

--- src/app/middlewares/ValidateMiddleware.ts
+++ src/app/middlewares/ValidateMiddleware.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../models/User';
 import { validate } from 'class-validator';
 import { getRepository } from 'typeorm';
+import transport from '../../config/main.init';
 class ValidateMiddleware {
     async validateSyntax(req: Request, res: Response, next: NextFunction) {
         try {
@@ -41,10 +42,20 @@ class ValidateMiddleware {
             if (!is_validated) {
                 user.email_token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
                 await repository.save(user);
+                await transport.sendMail({
+                    from: process.env.MAIL_USER,
+                    to: user.email,
+                    subject: "Please confirm your account",
+                    html: `<h1>Email Confirmation</h1>
+                        <h2>Hello User</h2>
+                        <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+                        <a href=${process.env.BASE_URL + "/validate_email/" + user.email_token}> Click here</a>
+                        </div>`,
+                  }).catch(err => console.log(err));
                 
+
                 return res.status(400).json({
                     error: "email is not validated",
-                    url: process.env.BASE_URL + "/validate_email/" + user.email_token
                 })
             }
 
