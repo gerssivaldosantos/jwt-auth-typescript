b4d55084495566b89dfd8fd5294d26ed942fed88
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-24 14:53:26 -0300

    feat: cors to acess by other app

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import 'reflect-metadata';
 import express from 'express';
 import './database/connect';
 import routes from './routes';
-
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(8085, () => console.log('😀 Server started on port 8085'));
